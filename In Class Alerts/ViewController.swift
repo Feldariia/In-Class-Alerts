8cebfc9947d0ecd004918f61e734fabdf6bbe11e
Shannon <s.mong@setonhill.edu>
2020-02-18 11:33:13 -0500

    Initial Commit

---
+++ In Class Alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  In Class Alerts
+//
+//  Created by Shannon Mong on 2/18/20.
+//  Copyright © 2020 Shannon Mong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
