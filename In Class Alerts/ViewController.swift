5826221ba3e71539d3e2fbb85131773d003b110c
Shannon <s.mong@setonhill.edu>
2020-03-19 11:40:14 -0400

    :)

--- In Class Alerts/ViewController.swift
+++ In Class Alerts/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+    
     }
+    @IBOutlet weak var usernameField: UITextField!
 
 
-}
+    @IBAction func displayAlert(_ sender: Any) {
+    
+    //Setup the alert
+        let alert = UIAlertController(title: "WARNING" ,message: "Are you in the right headspace to receive information that could possibly hurt you?", preferredStyle:.alert)
+        
+    //Add the action
+        alert.addAction(UIAlertAction(title: NSLocalizedString("idk", comment: "Default action"), style: .`default`))
+    
+        //Display rhe alert
+        self.present(alert,animated: true, completion: nil)
+    }
+    
+    @IBAction func playMedia(_ sender: Any) {
+        
+        //play .wav file
+        var soundID: SystemSoundID = 0
+        let soundFile:String=Bundle.main.path(forResource:"r2-d2" , ofType: "wav")!
+        let soundURL: NSURL = NSURL(fileURLWithPath: soundFile);AudioServicesCreateSystemSoundID(soundURL, &soundID);AudioServicesPlaySystemSound(soundID)
+        
+        
+        //Vibrate Phone
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+        
+    }
+   
+    
+    @IBAction func enterInfo(_ sender: Any) {
+        
+        //Design the input collection
+        let ac = UIAlertController(title: "Enter credentials for free Bitcoins", message: nil , preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "(✿◠‿◠)", style: .default) { [unowned ac] _ in
+            let answer = ac.textFields![0]
+            self.usernameField.text = answer.text
+       
+        //Design the second alert
+            let alert = UIAlertController(title: "you fool," , message: answer.text, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("oh", comment: "Default action" ), style: .`default`, handler: {_ in}))
+            
+            //Display the alert
+            self.present(alert,animated: true, completion: nil)}
+        ac.addAction(submitAction)
+        present(ac, animated:true)
+    
+        
+        }
+    }
 
